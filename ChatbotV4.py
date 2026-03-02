9d9365184ca7dd71257de3c7776e35850dcea489
Joyce Emília Mota <123253666+NegraEmilia71@users.noreply.github.com>
2026-03-02 18:02:05 -0300

    Create ChatbotV4.py

---
+++ ChatbotV4.py
@@ -0,0 +1,20 @@
+import openai
+openai.api_key = 'chave'
+
+def enviar_mensagem(mensagem, temperature):
+  resposta = openai.Completion.create(
+    engine='text-davinci-003',
+    prompt=mensagem,
+    max_tokens=50,
+    temperature=0.7,
+    n=1,
+    stop=None      
+  )
+  return resposta.choices[0].text.strip()
+
+mensagem_inicial = """
+Você: Olá, como posso te ajudar?
+Modelo: Eu sou um assistente virtual treinado para responder suas perguntas sobre produtos.
+"""
+mensagem_usuario = 'Qual é o seu nome?'
+resposta = enviar_mensagem(mensagem_usuario, temperature = 0.5)
